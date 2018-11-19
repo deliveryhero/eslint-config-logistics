27d4970b7c9715bd40e5a496f5c76649ea3693d8
Tagir Almasov <tccup1@gmail.com>
2018-11-19 16:16:18 +0100

    refactor(package.json): make repo commitizen friendly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,58 @@
 module.exports = {
-    'extends': [
-      'airbnb',
-      'prettier'
-    ],
-    'env': {
-      'browser': true,
-      'node': true,
-      'jest': true,
-      'es6': true
-    },
-    'parserOptions': {
-      'ecmaVersion': 6,
-      'sourceType': 'module',
-      'ecmaFeatures': {
-        'jsx': true
-      }
-    },
-    "plugins": [
-      "prettier"
-
-    ],
-    'rules': {
-      "prettier/prettier": "error",
-      'import/no-extraneous-dependencies': [
-        'error',
-        {
-          'devDependencies': true
-        }
-      ],
-      'max-len': [
-        'warn',
-        {
-          'code': 120,
-          'ignoreComments': true
-        }
-      ],
-      'quotes': [
-        'error',
-        'double',
-        'avoid-escape'
-      ],
-      'prefer-promise-reject-errors': 'warn',
-      'react/jsx-filename-extension': 'off',
-      'react/forbid-prop-types': 'error',
-      'import/prefer-default-export': 'off',
-      'import/no-default-export': 'error',
-      'import/no-named-as-default': 'warn',
-      'react/jsx-wrap-multilines': 'off',
-      'react/destructuring-assignment': [
-        'warn',
-        'always',
-        {
-          'ignoreClassFields': true
-        }
-      ]
+  'extends': [
+    'airbnb',
+    'prettier'
+  ],
+  'env': {
+    'browser': true,
+    'node': true,
+    'jest': true,
+    'es6': true
+  },
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true
     }
+  },
+  "plugins": [
+    "prettier"
+
+  ],
+  'rules': {
+    "prettier/prettier": "error",
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        'devDependencies': true
+      }
+    ],
+    'max-len': [
+      'warn',
+      {
+        'code': 120,
+        'ignoreComments': true
+      }
+    ],
+    'quotes': [
+      'error',
+      'double',
+      'avoid-escape'
+    ],
+    'prefer-promise-reject-errors': 'warn',
+    'react/jsx-filename-extension': 'off',
+    'react/forbid-prop-types': 'error',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/no-named-as-default': 'warn',
+    'react/jsx-wrap-multilines': 'off',
+    'react/destructuring-assignment': [
+      'warn',
+      'always',
+      {
+        'ignoreClassFields': true
+      }
+    ]
   }
+}
